1437e07ab82ef278e62d866a070b62b94078d2b4
AngeloRakotobe <angeloRakotobe@outlook.fr>
2024-05-02 23:53:37 +0300

    Mise a jour de la configuration de la connexion a MongoDB

--- config/MongoDB.js
+++ config/MongoDB.js
@@ -2,16 +2,12 @@ require("dotenv").config();
 
 const mongoose = require("mongoose") ;
 const uri = process.env.MONGO_URI ;
-const options = {
-    useNewUrlParser: true ,
-    useUnifiedTopology: true
-} ;
 
 const connection_mongoDB = async () => {
     try 
     {
         mongoose.set( "strictQuery" , false);
-        await mongoose.connect( uri , options );
+        await mongoose.connect( uri );
         console.log( "" ) ;
         console.log("Base de données connectée");
         console.log( "" ) ;
