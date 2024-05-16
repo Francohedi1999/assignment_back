f55b5623c1a61b68ae91523ed94dbbc5230fe4fe
AngeloRakotobe <angeloRakotobe@outlook.fr>
2024-05-17 00:30:15 +0300

    Modification des valeurs de retour des fonctions dans MatiereController

--- controllers/matiere.controller.js
+++ controllers/matiere.controller.js
@@ -25,46 +25,62 @@ exports.createMatiere = [
     async (req, res) => {
         try {
             const { nom, imageMatiere, idProf } = req.body;
+            const errors = [];
+            // Verification du champ nom
+            if (!nom || !nom.trim()) {
+                errors.push("Le nom de la matière est requis.");
+            }
+
+            // Verification du champ idProf
+            if (!idProf || !idProf.trim()) {
+                errors.push("Le professeur est requis.");
+            }
 
             // Check si l'ID du professeur is ObjectId valide
             if (!Utils.isValidObjectId(idProf)) {
-                return res.status(400).json({ message: "L'ID du professeur n'est pas valide." });
+                errors.push("L'ID du professeur n'est pas valide.");
             }
 
             // Check Doublon
             const existeDeja = await verificationDoublon(nom);
             if (existeDeja) {
-                return res.status(400).json({ message: "Une matière du même nom existe déjà." });
+                errors.push("Une matière du même nom existe déjà.");
             }
 
             if ( !req.files || Object.keys(req.files).length === 0 )
             {
-                return res.status(200).json( { message: "Aucun image uploadé." , created : false } ) ;
+                errors.push("Aucun image uploadé.");
             }
 
             const image = req.files.imageMatiere;
-            image.mv( path.join( "uploads/images/matieres", image.name), (error) => console.log(error) );
-            const file_url = BASE_URL + "/" + image.name ;
+            const cheminImage ="images/matieres";
+            image.mv( path.join( "uploads/" + cheminImage, image.name), (error) => console.log(error) );
+            const file_url = BASE_URL + "/" + cheminImage + "/" + image.name ;
 
             // Vérifier si l'utilisateur avec l'ID idProf existe dans la base de données et a le rôle de professeur
             const professeur = await User.findOne({ _id: idProf, role: 'professeur' });
             if (!professeur) {
-                return res.status(400).json({ message: "Le professeur saisie ne correspond pas à un profil professeur." });
+                errors.push("Le professeur saisi ne correspond pas à un profil professeur.");
             }
 
-            // Créer la matière avec le professeur associé
-            const nouvelleMatiere = new Matiere({
-                nom,
-                imageMatiere:file_url,
-                idProf
-            });
+            if (errors.length > 0){
+                return res.json({ errors,status:400 });
+            }else {
+                // Créer la matière avec le professeur associé
+                const nouvelleMatiere = new Matiere({
+                    nom,
+                    imageMatiere:file_url,
+                    idProf
+                });
 
-            // Créer la matière
-            const matiereEnregistree = await nouvelleMatiere.save();
-            return res.status(201).json({
-                message: "Matière créée avec succès!",
-                data: matiereEnregistree
-            });
+                // Créer la matière
+                const matiereEnregistree = await nouvelleMatiere.save();
+                return res.status(200).json({
+                    message: "Matière créée avec succès!",
+                    data: matiereEnregistree
+                });
+            }
+
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
@@ -76,33 +92,33 @@ exports.updateMatiere = [
         try {
             const { id } = req.params; // ID Matiere
             const { nom, idProf } = req.body;
-
+            const errors = [];
             // Verification du champ nom
             if (!nom || !nom.trim()) {
-                return res.status(400).json({ message: "Le nom de la matière est requis." });
+                errors.push("Le nom de la matière est requis.");
             }
 
             // Verification du champ idProf
             if (!idProf || !idProf.trim()) {
-                return res.status(400).json({ message: "Le professeur est requis." });
+                errors.push("Le professeur est requis.");
             }
 
             // Check si l'ID du professeur is ObjectId valide
             if (!Utils.isValidObjectId(idProf)) {
-                return res.status(400).json({ message: "L'ID du professeur n'est pas valide." });
+                errors.push("L'ID du professeur n'est pas valide.");
             }
 
             const matiereResult = await Matiere.findOne({ id: id });
 
             // Check si Matiere existe
             if (!matiereResult) {
-                return res.status(404).json({ message: "La matière n'existe pas !" });
+                errors.push("La matière n'existe pas !");
             }
 
             // Check Doublon
             const existeDeja = await verificationDoublonUpdate(id,nom);
             if (existeDeja) {
-                return res.status(400).json({ message: "Une matière du même nom existe déjà." });
+                errors.push("Une matière du même nom existe déjà.");
             }
 
             let file_url = matiereResult.imageMatiere;
@@ -110,27 +126,32 @@ exports.updateMatiere = [
             // Verifier si une nouvelle image importer
             if (req.files && req.files.imageMatiere) {
                 const image = req.files.imageMatiere;
-                image.mv(path.join("uploads/images/matieres", image.name), (error) => console.log(error));
-                file_url = BASE_URL + "/" + image.name;
+                const cheminImage ="images/matieres";
+                image.mv(path.join("uploads/" + cheminImage, image.name), (error) => console.log(error));
+                file_url = BASE_URL + "/" + cheminImage + "/" + image.name ;
             }
             // Check et mise a jour de l'ID du professeur
             const professeur = await User.findOne({ _id: idProf, role: 'professeur' });
             if (!professeur) {
-                return res.status(400).json({ message: "Le professeur saisie ne correspond pas à un profil professeur." });
+                errors.push("Le professeur saisi ne correspond pas à un profil professeur.");
             }
 
+            if (errors.length > 0){
+                return res.json({ errors,status:400 });
+            }else{
+                // Mettre a jour Matière
+                matiereResult.nom = nom;
+                matiereResult.imageMatiere = file_url;
+                matiereResult.idProf = idProf;
 
-            // Mettre a jour Matière
-            matiereResult.nom = nom;
-            matiereResult.imageMatiere = file_url;
-            matiereResult.idProf = idProf;
+                // Save modifications
+                const matiereMiseAJour = await matiereResult.save();
+                return res.status(200).json({
+                    message: "Matière mise à jour avec succès!",
+                    data: matiereMiseAJour
+                });
+            }
 
-            // Save modifications
-            const matiereMiseAJour = await matiereResult.save();
-            return res.status(200).json({
-                message: "Matière mise à jour avec succès!",
-                data: matiereMiseAJour
-            });
         } catch (error) {
             res.status(500).json({ message: error.message });
         }
